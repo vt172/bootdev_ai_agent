4fafed45625a0b7284a37116802d7d6f43fdd636
vt172 <park.philippe@gmail.com>
2025-06-10 21:05:32 +0200

    CH1L6: Verbose
    Verbose option added.
    I also added it to the usage that runs if there are no parameters set.

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-import os, sys
+import os, sys, pathlib
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     model = "gemini-2.0-flash-001"
+
     if len(sys.argv)>1:
-        contents = sys.argv[1]
+        user_prompt = sys.argv[1]
     else:
-        print("Please provide an argument.\nExemple: python3 main.py \'your prompt\'")
+        with open("config/usage") as f:
+            print(f.read())
         sys.exit(1)
 
-    response = client.models.generate_content(model=model,contents=contents)
-    print(response.text)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),    
+    ]
+
     print("...")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    response = client.models.generate_content(model=model,contents=messages)
+    print(response.text)
+    if len(sys.argv)>2 and sys.argv[2] == "--verbose":
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+
 
 if __name__ == "__main__":
     main()
